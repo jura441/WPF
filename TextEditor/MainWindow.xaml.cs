aca790919c0864dd80032f23d816c13b2caf4f4e
Юрий Иванов <jura401@mail.ru>
2022-11-18 21:27:32 +0300

    181122

---
+++ TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void main_mi_create_Click(object sender, RoutedEventArgs e)
+        {
+            rtb_editor.Document.Blocks.Clear();
+
+            //TextRange textRange = new TextRange(paragraph.ContentStart, paragraph.ContentEnd);
+             
+
+            rtb_editor.Document.Blocks.Add(new Paragraph(new Run("я новый документ")));
+        }
+
+        private void main_mi_open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void main_mi_save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void main_mi_saveAs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void main_mi_settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void main_mi_print_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void main_mi_exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
